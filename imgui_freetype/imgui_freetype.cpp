827b991505b705f398675d216357292c36acddf0
omar <omarcornut@gmail.com>
2018-01-23 09:23:01 +0100

    imgui_freetype: fix for addition of ImFontAtlas::TexUvscale member

--- imgui_freetype/imgui_freetype.cpp
+++ imgui_freetype/imgui_freetype.cpp
@@ -7,6 +7,7 @@
 // - v0.51: (2017/08/26) cleanup, optimizations, support for ImFontConfig::RasterizerFlags, ImFontConfig::RasterizerMultiply.
 // - v0.52: (2017/09/26) fixes for imgui internal changes
 // - v0.53: (2017/10/22) minor inconsequential change to match change in master (removed an unnecessary statement)
+// - v0.54: (2018/01/22) fix for addition of ImFontAtlas::TexUvscale member
 
 // Todo/Bugs:
 // - Font size has lots of waste.
@@ -243,7 +244,8 @@ bool ImGuiFreeType::BuildFontAtlas(ImFontAtlas* atlas, unsigned int extra_flags)
 
     atlas->TexID = NULL;
     atlas->TexWidth = atlas->TexHeight = 0;
-    atlas->TexUvWhitePixel = ImVec2(0, 0);
+    atlas->TexUvScale = ImVec2(0.0f, 0.0f);
+    atlas->TexUvWhitePixel = ImVec2(0.0f, 0.0f);
     atlas->ClearTexData();
 
     ImVector<FreeTypeFont> fonts;
@@ -287,6 +289,7 @@ bool ImGuiFreeType::BuildFontAtlas(ImFontAtlas* atlas, unsigned int extra_flags)
 
     // Create texture
     atlas->TexHeight = ImUpperPowerOfTwo(atlas->TexHeight);
+    atlas->TexUvScale = ImVec2(1.0f / atlas->TexWidth, 1.0f / atlas->TexHeight);
     atlas->TexPixelsAlpha8 = (unsigned char*)ImGui::MemAlloc(atlas->TexWidth * atlas->TexHeight);
     memset(atlas->TexPixelsAlpha8, 0, atlas->TexWidth * atlas->TexHeight);
 
